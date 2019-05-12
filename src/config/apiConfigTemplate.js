f185c7d89e18a9419de589948aeb7209d99c1ca5
Bliss Man <blissman@gmail.com>
2019-05-12 09:28:12 -0400

    add apiConfigTemplate.js examples for setting up the apiConfig object

---
+++ src/config/apiConfigTemplate.js
@@ -0,0 +1,9 @@
+// include your api keys here for what3words (https://www.what3words.com) and unsplash (https://www.unsplash.com)
+const apiConfig = {
+    "what3words": {
+        "accessKey": "<your-access-key>"
+    },
+    "unsplash": {
+        "accessKey": "<your-access-key>"
+    }
+};
