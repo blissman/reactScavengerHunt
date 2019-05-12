17a99310e2275de5a0fbfc48ff1b44d3bc6559f6
Bliss Man <blissman@gmail.com>
2019-05-12 15:25:08 -0400

    add get location button component, add test for display

---
+++ src/components/GetLocationButton.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function GetLocationButton() {
+    return (
+        <div id="GetLocationButton">
+        <button type="button" onclick="alert('get location')">Get Location</button>
+        </div>
+    );
+}
+
+export default App;
