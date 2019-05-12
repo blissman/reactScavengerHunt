ea0b6582f70283781c35b093e83fe633f95b1199
Bliss Man <blissman@gmail.com>
2019-05-12 09:35:39 -0400

    beautify

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-      <body className="App-body">
-      <p>Figure out what to do here . . . </p>
-      </body>
-    </div>
-  );
+    return (
+        <div className="App">
+        <header className="App-header">
+        </header>
+        <body className="App-body">
+        <p>Figure out what to do here . . . </p>
+        </body>
+        </div>
+    );
 }
 
 export default App;
